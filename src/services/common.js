3ad0a743c0cc9eba2bdab520e90a593f31646ae4
Abhishek mishra <abhishekmishra992016@gmail.com>
2024-05-20 10:11:49 +0530

    tiff file working

--- src/services/common.js
+++ src/services/common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const REACT_APP_IP = "192.168.0.106";
+export const REACT_APP_IP = "192.168.0.189";
+// export const REACT_APP_IP = "192.168.0.106";
 
 export const onGetTemplateHandler = async () => {
   const token = JSON.parse(localStorage.getItem("userData"));
